638c58926a97ee95c3da4d5f187a015f216e1db8
himanshu0072 <himanshu2003prajapati@gmail.com>
2024-10-02 01:28:34 +0530

    Fixing UI

--- src/assets/Components/FetchTasks.jsx
+++ src/assets/Components/FetchTasks.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 function FetchTasks({ tasks, deleteTask }) {
   return (
-    <div className="mt-4 mb-5">
+    <div className="mt-4">
       <div className="innerBox rounded bg-light text-dark p-4 shadow-sm">
         <h1 className="text-center fs-3 mb-4">Todays Tasks</h1>
         <div className="d-flex flex-column">
@@ -14,7 +14,7 @@ function FetchTasks({ tasks, deleteTask }) {
             tasks.map((task, index) => (
               <div
                 key={index}
-                className="d-flex justify-content-between align-items-center border rounded p-3 mb-2 bg-white"
+                className="d-flex justify-content-between align-items-center border rounded p-3 mb-2 bg-white flex-wrap"
               >
                 <div className="d-flex flex-grow-1">
                   <span className="fs-5 mx-2">{index + 1}.</span>
@@ -23,7 +23,7 @@ function FetchTasks({ tasks, deleteTask }) {
                 </div>
                 <button
                   type="button"
-                  className="btn btn-danger"
+                  className="btn btn-danger mt-2 mt-md-0" // Add margin top for mobile
                   onClick={() => deleteTask(index)}
                 >
                   Delete
